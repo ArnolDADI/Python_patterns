f00491e273d77392bdbe2f550785127d36e2ffdb
ArnolDADI <arnoldredcliffe@gmail.com>
2023-09-25 21:03:09 +0530

    uploading result

--- result.py
+++ result.py
@@ -2,12 +2,13 @@
 To print all the pattern files starting with 'pattern_' in its name.
 """
 import os
-with open('pattern.md', 'w', encoding = 'utf-8') as f:
+
+# To create a new file every time the file is run and check for replaced, added or removed files
+with open('pattern.txt', 'w', encoding = 'utf-8') as f:
     for x in os.listdir():
         if x.startswith("pattern_"):
-            # To get the name of the file
-            patterns = x[:-3]
-            
+            patterns = x[:-3]       # To get the name of the file
+
             try:
                 mod =__import__(patterns)
                 f.write(f"\n\n*{patterns}\n")
